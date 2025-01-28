4fc5a900aab8d1c85a82f2080f4acf82a3d3b8f5
Grayden Shand <graydenshand@gmail.com>
2025-01-28 09:59:26 -0500

    Remove unused variable

--- tests/test_code_capture.py
+++ tests/test_code_capture.py
@@ -1,7 +1,7 @@
 from code_capture import CodeCapture
 
 def test_code_is_captured():
-    with CodeCapture("hello") as cc:
+    with CodeCapture("hello"):
         name = "world"
         print(f"Hello, {name}!")
     assert CodeCapture.store["hello"] == "name = \"world\"\nprint(f\"Hello, {name}!\")\n"
